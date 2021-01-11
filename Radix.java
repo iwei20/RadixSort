87611d3925a3f368eff097dff87a8f46e561f9fc
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-01-11 13:00:39 -0500

    Implement 3

--- Radix.java
+++ Radix.java
@@ -22,4 +22,10 @@ public class Radix {
         return Math.max(1, result);
     }
 
+    public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+        for(int i = 0; i < buckets.length; ++i) {
+            original.extend(buckets[i]);
+        }
+    }
+
 }
