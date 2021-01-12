497796a3dad9f10cc3681de6e192dc2afe1b60de
iwei20 <44007607+ivanwei20@users.noreply.github.com>
2021-01-12 11:07:58 -0500

    Change to SortableLinkedList

--- Radix.java
+++ Radix.java
@@ -22,7 +22,7 @@ public class Radix {
         return Math.max(1, result);
     }
 
-    public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+    public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
         for(int i = 0; i < buckets.length; ++i) {
             original.extend(buckets[i]);
         }
